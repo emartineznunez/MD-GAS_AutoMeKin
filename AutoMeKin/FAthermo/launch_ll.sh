56b90c262a6b9d1c56df3f426f84d4890fd01e73
Emilio Martínez Núñez <50674314+emartineznunez@users.noreply.github.com>
2022-11-21 10:36:10 +0100

    Update launch_ll.sh

--- AutoMeKin/FAthermo/launch_ll.sh
+++ AutoMeKin/FAthermo/launch_ll.sh
@@ -1,5 +1,7 @@
-module load cesga/2020 gcccore/system automekin/2021 
-sbatch -n 10 -t 00:10:00 llcalcs.sh FAthermo.dat 10 5
+#!/bin/bash
+
+module load automekin/2021 
+sbatch -A biosim_serv --nodes=1 --ntasks=10  --partition=biosim -t 0-00:10:00 llcalcs.sh FA.dat 10 2
 
 kinetics.sh 5000 ll > htemp.log
 
